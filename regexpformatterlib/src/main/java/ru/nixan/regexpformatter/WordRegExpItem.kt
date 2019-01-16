9002e2b2c6ae21319a26b245f5954e4e63c7abb5
Ilya Linnik <i.linnik@rambler-co.ru>
2019-01-16 12:01:08 +0300

    Fixed a bug in word regular expression item

--- regexpformatterlib/src/main/java/ru/nixan/regexpformatter/WordRegExpItem.kt
+++ regexpformatterlib/src/main/java/ru/nixan/regexpformatter/WordRegExpItem.kt
@@ -1,12 +1,10 @@
 package ru.nixan.regexpformatter
 
-import android.text.Editable
-
 class WordRegExpItem(override val length: Length) : RegExpItem {
 
     override fun toString() = "\\w$length"
 
-    override fun format(input: Editable, startPosition: Int, endPosition: Int) = (0 until endPosition - startPosition).takeWhile { length.compareWithPosition(it) <= 0 }.count()
+    override fun format(input: RegularExpressionSpannable, startPosition: Int, endPosition: Int) = (0 until endPosition - startPosition).takeWhile { length.compareWithPosition(it) <= 0 }.count()
 
     override fun matches(string: String, startPosition: Int, endPosition: Int): RegExpItem.MatchResult {
         if (string.length < startPosition) {
